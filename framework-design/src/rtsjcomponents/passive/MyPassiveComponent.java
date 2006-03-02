8f0f6c9b63bc30f32e92116693752a39c04960b4
Juan Colmenares <juancol@doc.ece.uci.edu>
2006-03-02 19:13:19 +0000

    just the interface
    git-svn-id: file:///Users/kraman/Documents/personal/doc/compadres@3993 bfff1265-3dd4-0310-90a8-ce2828190deb

---
+++ framework-design/src/rtsjcomponents/passive/MyPassiveComponent.java
@@ -0,0 +1,16 @@
+package rtsjcomponents.passive;
+
+/**
+ * @author juancol
+ */
+public interface MyPassiveComponent {
+    
+    int execSIM_0(int i);
+    
+    Integer execSIM_1(int i);
+    
+    int execSDM_0(int i);
+    
+    Integer execSDM_1(int i);
+    
+}

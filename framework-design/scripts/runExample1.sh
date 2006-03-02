44095d17e659427eee8bdfdf5f7ab2d2ea28c129
Juan Colmenares <juancol@doc.ece.uci.edu>
2006-03-02 03:42:40 +0000

    a little reorganization
    git-svn-id: file:///Users/kraman/Documents/personal/doc/compadres@3992 bfff1265-3dd4-0310-90a8-ce2828190deb

--- framework-design/scripts/runExample1.sh
+++ framework-design/scripts/runExample1.sh
@@ -1,6 +1,6 @@
 #/bin/bash
 HOMEDIR=/opt/timesys/rtsj-ri
-BIN=/home/shruti/test/compadres/framework-design/bin
+BIN=../bin
 export LD_ASSUME_KERNEL=2.4.1; 
 
 $HOMEDIR/bin/tjvm -Djava.class.path=$BIN -Xbootclasspath=$HOMEDIR/lib/foundation.jar rtsjcomponents.example1.Example1

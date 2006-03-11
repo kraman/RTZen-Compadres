75b9fd5ec3f8dbf9bdf4ce2a2cd6961165f5187a
Juan Colmenares <juancol@doc.ece.uci.edu>
2006-03-11 13:40:37 +0000

    multiruns with jRate too
    git-svn-id: file:///Users/kraman/Documents/personal/doc/compadres@4025 bfff1265-3dd4-0310-90a8-ce2828190deb

--- framework-design/scripts/multiRun.sh
+++ framework-design/scripts/multiRun.sh
@@ -5,7 +5,7 @@ export LD_ASSUME_KERNEL=2.4.1;
 
 DIR_BASENAME=TEST
 MAX_TESTCASES=5
-MAX_RUNS=10
+MAX_RUNS=2
 
 
 if [ "$1" = "jrate" ]; then
@@ -15,7 +15,7 @@ elif [ "$1" = "tjvm" ]; then
   echo "SCRIPT: Running tjvm!"
   CMD="$HOMEDIR/bin/tjvm -Djava.class.path=$BIN -Xbootclasspath=$HOMEDIR/lib/foundation.jar rtsjcomponents.example2.Example2"
 elif [ "$1" = "clean" ]; then
-  rm -Rf "$DIR_BASENAME"*
+  rm -Rf *_"$DIR_BASENAME"*
   exit
 else
   echo "Usage: multiRun.sh [jrate|tjvm|clean]";
@@ -34,7 +34,7 @@ do
        R=`expr $R + 1`;
     done;
     R=0;    
-    OUTPUT_DIR=./"$DIR_BASENAME"_"$T";
+    OUTPUT_DIR=./"$1"_"$DIR_BASENAME"_"$T";
     mkdir $OUTPUT_DIR
     mv *.txt $OUTPUT_DIR/.
     T=`expr $T + 1`;
